843043eade8ec570aabc8cd11f31581bb633a7d0
Valentin Galfre <valentin@vrio.com>
2023-02-16 12:28:03 -0300

    Adding title to _app header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <Head>
+        <meta name='title' content='Habitual' />
+        <title>Habitual</title>
         <meta
           name='viewport'
           content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
