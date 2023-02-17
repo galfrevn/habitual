a925dd60b88e44e474d10c626cb5a0e51c98a3f9
Valentin Galfre <galfre.vn@gmail.com>
2023-02-17 14:51:57 -0300

    Fixing dark mode in header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class', '[data-theme="dark"]'],
+  darkMode: ['class'],
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
     fontFamily: {
