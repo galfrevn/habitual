f556fd6a06a008dfa44e7565453e57818aabbf66
Valentin Galfre <galfre.vn@gmail.com>
2023-02-17 14:34:07 -0300

    Fixing darkmode styles in navigation bar

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import { ThemeSwitcher } from 'components/home/theme-switcher';
 import { Navigation } from 'components/layout/header/navigation';
 import { Profile } from 'components/layout/header/profile';
+import { useRouter } from 'next/router';
+
+const bannedRoutes = ['/auth'];
 
 export function Header() {
+  const { pathname } = useRouter();
+  if (bannedRoutes.includes(pathname)) return null;
+
   return (
-    <header className='sticky top-0 z-40 w-full border-b border-b-slate-200 bg-white dark:border-b-neutral-700 dark:bg-neutral-900/80'>
+    <header className='sticky top-0 z-40 w-full border-b border-b-neutral-200 bg-white dark:border-b-neutral-700 dark:bg-neutral-900/80'>
       <div className='container flex h-16 items-center'>
         <Navigation />
+        <ThemeSwitcher />
         <Profile />
       </div>
     </header>
